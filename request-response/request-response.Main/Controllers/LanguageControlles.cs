5291e9b7c43a8a80eaf7ca16d020818290c7713a
Annefleur Hay <annefleurhay1@gmail.com>
2023-09-26 18:06:58 +0200

    Added core

--- request-response/request-response.Main/Controllers/LanguageControlles.cs
+++ request-response/request-response.Main/Controllers/LanguageControlles.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+using request_response.Models;
 
 namespace request_response.Controllers
 {
@@ -7,8 +9,75 @@ namespace request_response.Controllers
     [Route("[controller]")]
     public class LanguageController : ControllerBase
     {
+        private static List<Language> _languages = new List<Language>();
+
+
         public LanguageController()
         {
+
+            if(_languages.Count == 0)
+            {
+                _languages.Add(new Language() { Name = "Javascript"});
+            }
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [Route("Create")]
+        public async Task<IResult> CreateStudent(Language language)
+        {
+            _languages.Add(language);
+            return Results.Created("boolean.com", language);
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("All")]
+        public async Task<IResult> GetAll()
+        {
+            return Results.Ok(_languages);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("One")]
+        public async Task<IResult> GetOne(string name)
+        {
+            var oneLanguage = _languages.Where(l => l.Name == name).FirstOrDefault();
+            return oneLanguage != null ? Results.Ok(oneLanguage) : Results.NotFound();
+
+        }
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [Route("Update")]
+        public async Task<IResult> Update(string name, Language newLanguage)
+        {
+
+            var updateLanguage = _languages.Where(l => l.Name == name).FirstOrDefault();
+            if (updateLanguage == null) return Results.NotFound();
+
+
+            updateLanguage.Name = string.IsNullOrEmpty(newLanguage.Name) ? updateLanguage.Name : newLanguage.Name;
+
+            return Results.Created("boolean.com", updateLanguage);
+
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("Delete")]
+        public async Task<IResult> Delete(string name)
+        {
+            var language = _languages.Where(l => l.Name == name).FirstOrDefault();
+            int result = _languages.RemoveAll(l => l.Name == name);
+            return result >= 0 && language != null ? Results.Ok(name) : Results.NotFound();
         }
     }
 }
--- request-response/request-response.Main/Controllers/StudentController.cs
+++ request-response/request-response.Main/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using request_response.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace request_response.Controllers
 {
@@ -7,8 +9,80 @@ namespace request_response.Controllers
     [Route("[controller]")]
     public class StudentController : ControllerBase
     {
+
+        private static List<Student> _students = new List<Student>();
         public StudentController()
         {
+            if(_students.Count == 0) {
+                _students.Add(new Student() { firstName="Nathan", lastName="King"});
+                
+                
+            }
         }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [Route("Create")]
+        public async Task<IResult> CreateStudent(Student student)
+        {
+            _students.Add(student);
+            return Results.Created("boolean.com", student);
+            
+
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("All")]
+        public async Task<IResult> GetAll()
+        {
+            return Results.Ok(_students);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("One")]
+        public async Task<IResult> GetOne(string first)
+        {
+            var student = _students.Where(s => s.firstName == first).FirstOrDefault();
+            return student != null ? Results.Ok(student) : Results.NotFound();
+            
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [Route("Update")]
+        public async Task<IResult> Update(string firstName, Student newStudent)
+        {
+
+            var student = _students.Where(s => s.firstName == firstName).FirstOrDefault();
+            if (student == null) return Results.NotFound();
+            
+
+            student.firstName = string.IsNullOrEmpty(newStudent.firstName) ? student.firstName : newStudent.firstName;
+            student.lastName = string.IsNullOrEmpty (newStudent.lastName) ? student.lastName : newStudent.lastName;
+
+
+            return Results.Created("boolean.com", student);
+            
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("Delete")]
+        public async Task<IResult> DeleteStudent(string firstName)
+        {
+            var student = _students.Where(s => s.firstName == firstName).FirstOrDefault();
+            int result = _students.RemoveAll(s => s.firstName == firstName);
+            return result >= 0 && student!= null ? Results.Ok(student) : Results.NotFound();
+        }
+
+
+
     }
 }
